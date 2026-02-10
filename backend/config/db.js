c5a71610947e3686d54b58f1d377f97ab12bf77d
Aditya Malguri <malguriaditya825@gmail.com>
2026-02-10 23:18:42 +0530

    [third commit]

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,5 +5,5 @@ import mongoose  from "mongoose";
 
 
 export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://FoodDel:asdfgasdfgasdfgasdfg@cluster0.15hyjdo.mongodb.net/food-del').then(()=>console.log("db connected"));
+    await mongoose.connect(process.env.MONGO_DB_URL).then(()=>console.log("db connected"));
 }
